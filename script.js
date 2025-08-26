399bc8e4b64ad9312807a082853c76e88f05d7cb
suellen-sususapeca <133366118+suellen-sususapeca@users.noreply.github.com>
2025-08-26 11:38:30 -0300

    2608

--- script.js
+++ script.js
@@ -1,36 +1,113 @@
-const caixaPrincipal = document.querySelector("caixa-principal")
-const caixaPerguntas = document.querySelector("caixa-pergunta")
-const caixaAlternativas = document.querySelector("caixa-alternativas")
-const caixaResultado = domcument.querySelector("caixa-resultado")
-const textoResultado = domcument.querySelector("texto-resultado")
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Você se depara com uma oportunidade incrivel de conhecer uma nova cultura. Qual destino ia chegar"
-            alternativas: [
-                {
-            texto: "Inglaterra",
-            afirmacao:"afirmação",
-                },
-                {
-            texto:"Estados unidos",
-            afirmacao:"afirmação",
-                },
-            ]
-        },
+        enunciado: "Certo dia duas meninas foram passear num campo , logo avistaram dois caminhos diferentes ",
+        alternativas: [
+            {
+                texto: "Elas escolheram ir para a esquerdo!",
+                afirmacao: "O caminho estava todo escuro e cheio de espinhos nas flores , elas ficaram com muito medo. "
+            },
+            {
+                texto: "Elas escolheram ir para a direito!",
+                afirmacao: "O caminho estava todo iluminado e cheio de flores."
+            }
+        ]
+    },
+    {
+        enunciado: "Depois que elas passaram esse caminho , logo ouviram um barulho muito alto de um animal , e elas ficaram com muito medo ",
+        alternativas: [
+            {
+                texto: "Elas decidiram correr.",
+                afirmacao: "O animal ouviu elas correr e correu atras delas."
+            },
+            {
+                texto: "Elas decidiram se esconder.",
+                afirmacao: "Elas avistaram uma casa abandonada."
+            }
+        ]
+    },
+    {
+        enunciado: "E agora o que irá acontecer?",
+        alternativas: [
+            {
+                texto: "Elas decidiram entrar na casa .",
+                afirmacao: "."
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+        ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
+    },
+];
 
-    let atual = 0;
-    let pergunatAtual;
 
-    function mostraPergunta (){
-        perguntaAtual = pergunta [atual];
-        caixaPerguntas.textContent = perguntas.enunciados;
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
     }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
 
-    mostraPergunta ();
-    
-        
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
 
-        
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
 
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
+mostraPergunta();
