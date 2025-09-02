5fc02eba90a9c64f89ba8251dd7a2c3da489f833
suellen-sususapeca <133366118+suellen-sususapeca@users.noreply.github.com>
2025-09-02 11:20:26 -0300

    0209

--- script.js
+++ script.js
@@ -32,41 +32,41 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "E agora o que irá acontecer?",
+        enunciado: "E agora o que elas podem fazer ?",
         alternativas: [
             {
-                texto: "Elas decidiram entrar na casa .",
-                afirmacao: "."
+                texto: "Elas acharam um outro caminho.",
+                afirmacao: "Mas elas acabaram se perdendo"
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Elas decidiram entrar na casa.",
+                afirmacao: "A casa esta segura para passarem a noite ."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: " Ajude elas a tomar uma decisão sobre o que fazer !",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Elas decidiram andar por mais tempo.",
+                afirmacao: "Elas andaram por muito tempo e acharam o caminho certo."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Elas foram embora da casa abondonada .",
+                afirmacao: "Elas acharam o caminho certo"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Elas encontraram um carro no caminho",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Elas decidiram não entrar.",
+                afirmacao: "Elas andaram o caminho todo apê até a casa delas."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Elas decidiram entrar no carro.",
+                afirmacao: "Elas fizeram uma ligação com os fios para ligar o carro e foram para suas casas. "
             }
         ]
     },
